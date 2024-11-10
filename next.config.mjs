5e82d8e734e27b904e9e1d8ec325d01498256cc6
Wojciech Polak <wojciechpolak@gmail.com>
2024-11-10 20:44:47 +0100

    Add PWA

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
+import withSerwistInit from '@serwist/next';
+
+const withSerwist = withSerwistInit({
+  cacheOnNavigation: true,
+  swSrc: 'src/app/sw.ts',
+  swDest: 'public/sw.js',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'standalone',
 };
 
-export default nextConfig;
+export default withSerwist(nextConfig);
